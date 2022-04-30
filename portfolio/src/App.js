e9486b0faa6c610b5dfee83d78a996a7d674d0b2
Pratima <gaitrisanu2468@gmail.com>
2022-04-30 16:05:42 +0545

    some changes in route
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 import Layout from './components/Layout'
-import Home from './components/Home';
+import './App.scss'
 
 function App() {
   return (
-  <> 
-  <Routes><Route path="/" element={<Layout />} />
-      <Route path="/" element={<Home />} />
-      
-     </Routes> 
-  </>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
