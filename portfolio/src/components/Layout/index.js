b26f88d6dcb8d572c324b123eb4ade03fc49b40c
Pratima <gaitrisanu2468@gmail.com>
2022-04-30 21:35:12 +0545

    add layout
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -1,23 +1,21 @@
-import './index.scss'
 import { Outlet } from 'react-router-dom'
-import React from 'react'
-import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar/'
+import './index.scss'
 
 const Layout = () => {
   return (
-    <div className='App'>
-    <Sidebar />
-    <div className='page'>
-    <span className='tags top-tags'>&lt;body&gt;</span>
-    
-    <Outlet />
-    
-    <span className='tags bottom-tags'>
-    &lt;/body&gt;
-    <br />
-    <span className='bottom-tag-html'>&lt;/html&gt;</span>
-    </span>
-    </div>
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
   )
 }
